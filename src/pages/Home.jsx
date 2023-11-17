a1a6882bb069508edfb39830f896397a9af1869b
santos-eric <esantosdev77@gmail.com>
2023-11-17 04:55:38 -0700

    basic lighting added at home and loading screen added with react suspense

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import Loader from '../components/Loader'
+
+{/* <div className="absolute top-28 left-0 right-0 z-10 flex items-cener justify-center">
+        PopUp
+      </div> */}
 
 const Home = () => {
   return (
-    <div>Home</div>
+    // full width and height
+    <section className="w-full h-screen relative">
+      {/* root component for 3D Scene */}
+      <Canvas
+      className='w-full h-screen bg-transparent'
+      // near 0.1 and further than 1000 won't be rendered
+      camera={{near: 0.1, far: 1000}}
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight/>
+        </Suspense>
+      </Canvas>
+    </section>
   )
 }
 
