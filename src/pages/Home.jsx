6a64f2da9729b15f2d92c81f798b579fd33387e8
santos-eric <esantosdev77@gmail.com>
2023-11-21 05:15:03 -0700

    added function to resize 3D object on mobile and used as properties on island component at home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,12 +10,33 @@ import Island from '../models/Island'
       </div> */}
 
 const Home = () => {
+
+  // set island default position and make it mobile getting the screeSize from window.innerWidth
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null
+    let screenPosition = [0, -6.5, -43]
+
+    if (window.innerWidth < 768) {
+      // [x,y,z] scale
+      screenScale = [0.9, 0.9, 0.9]
+    } else {
+      screenScale = [1, 1, 1]
+    }
+
+    return [screenScale, screenPosition]
+  }
+
+  // destructure the function to use as island component properties
+  const [islandScale, islandPosition] = adjustIslandForScreenSize()
+
   return (
     // full width and height
     <section className="w-full h-screen relative">
+
       {/* root component for 3D Scene */}
       <Canvas
-      className='w-full h-screen bg-transparent'
+        className='w-full h-screen bg-transparent'
+
       // near 0.1 and further than 1000 won't be rendered
       camera={{near: 0.1, far: 1000}}
       >
@@ -26,7 +47,10 @@ const Home = () => {
           <spotLight />
           <hemisphereLight />
 
-          <Island/>
+          <Island
+          position={islandPosition}
+          scale={islandScale}
+          />
         </Suspense>
       </Canvas>
     </section>
